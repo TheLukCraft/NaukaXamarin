2c913ea5a624099b64df9258ccd6fbf608f6d439
TheLukCraft <luk1997r@o2.pl>
2020-12-03 02:55:34 +0100

    ContactGroup

---
+++ NaukaXamarin/Models/ContactGroup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaukaXamarin.Models
+{
+    class ContactGroup : List<Contact>
+    {
+        public string Title { get; set; }
+        public string ShortTitle { get; set; }
+    }
+}
